e889801850e274c4c46a7e6e357b64b1034f4012
Namakudami <63004861+Namakudami@users.noreply.github.com>
2020-11-07 11:55:24 -0500

    Create web19201.jsx

---
+++ web19201/web19201.jsx
@@ -0,0 +1,38 @@
+
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./web19201.css";
+
+class Web19201 extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+
+      };
+  }
+  
+  render() {
+    
+    return (
+          <div data-layer="95caca8e-ee6c-4a4c-aee2-a76c077025f3" className="web19201">        <div data-layer="e9ea14ed-dc6f-4712-85f1-e9851f7b7737" className="fireblade">Fireblade</div>
+        <div data-layer="088b5a5b-3d84-4747-a862-92b0646f4365" className="rectangle1"></div>
+        <svg data-layer="84f10da5-99c3-4bf6-9ece-5b29c6a70628" preserveAspectRatio="none" viewBox="-10.5 -10.5 176 180" className="ellipse1"><path d="M 77.5 0 C 120.3020629882813 0 155 35.59336471557617 155 79.5 C 155 123.4066314697266 120.3020629882813 159 77.5 159 C 34.69793319702148 159 0 123.4066314697266 0 79.5 C 0 35.59336471557617 34.69793319702148 0 77.5 0 Z"  /></svg>
+</div>
+
+    );
+  }
+}
+
+Web19201.propTypes = {
+
+}
+
+Web19201.defaultProps = {
+
+}
+
+
+export default Web19201;
+          
