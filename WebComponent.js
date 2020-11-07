5e72b6d7cdc973bb9398360f3a2546e83a1c23c1
Namakudami <63004861+Namakudami@users.noreply.github.com>
2020-11-07 12:06:29 -0500

    might work

---
+++ WebComponent.js
@@ -0,0 +1,41 @@
+/**
+ *
+ * This file was generated with Adobe XD React Exporter
+ * Exporter for Adobe XD is written by: Johannes Pichler <j.pichler@webpixels.at>
+ *
+ **/
+
+import React from "react";
+
+const WebComponent = () => (
+  <svg width={1920} height={1080} viewBox="0 0 1920 1080">
+    <defs>
+      <style>
+        {
+          ".a{clip-path:url(#b);}.b{fill:#707070;font-size:100px;font-family:SegoeUI, Segoe UI;}.c,.d{stroke:#707070;}.d{fill:#8b1919;stroke-width:14px;}.e{stroke:none;}.f{fill:none;}.g{fill:#fffbfb;}"
+        }
+      </style>
+      <clipPath id="b">
+        <rect width={1920} height={1080} />
+      </clipPath>
+    </defs>
+    <g id="a" className="a">
+      <rect className="g" width={1920} height={1080} />
+      <text className="b" transform="translate(50 64)">
+        <tspan x={0} y={108}>
+          {"Fireblade"}
+        </tspan>
+      </text>
+      <g className="c" transform="translate(729 346)">
+        <rect className="e" width={572} height={321} />
+        <rect className="f" x={0.5} y={0.5} width={571} height={320} />
+      </g>
+      <g className="d" transform="translate(166 602)">
+        <ellipse className="e" cx={77.5} cy={79.5} rx={77.5} ry={79.5} />
+        <ellipse className="f" cx={77.5} cy={79.5} rx={70.5} ry={72.5} />
+      </g>
+    </g>
+  </svg>
+);
+
+export default WebComponent;
